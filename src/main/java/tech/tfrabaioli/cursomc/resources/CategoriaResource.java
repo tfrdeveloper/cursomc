b23b8b06eaa42f2caf8fd9f10004f7179a779e91
tfrdeveloper <tfrabaioli@outlook.com>
2022-06-13 13:25:49 +0100

    Rest Ok

---
+++ src/main/java/tech/tfrabaioli/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package tech.tfrabaioli.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String list() {
+		return "Rest Ok";
+	}
+
+}
