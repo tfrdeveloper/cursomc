9cc07f130e020e90af7cf1ddc11a4edd0bb7d6f0
tfrdeveloper <tfrabaioli@outlook.com>
2022-07-03 13:32:26 +0100

    CommandLineRunner

--- src/main/java/tech/tfrabaioli/cursomc/CursomcApplication.java
+++ src/main/java/tech/tfrabaioli/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package tech.tfrabaioli.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tech.tfrabaioli.cursomc.domain.Categoria;
+import tech.tfrabaioli.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
