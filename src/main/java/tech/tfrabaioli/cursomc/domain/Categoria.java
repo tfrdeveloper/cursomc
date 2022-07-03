960f3d3da35d6ec0155d4d234f23f10410797df7
tfrdeveloper <tfrabaioli@outlook.com>
2022-07-03 13:10:39 +0100

    Categiria Class and h2-console DB

---
+++ src/main/java/tech/tfrabaioli/cursomc/domain/Categoria.java
@@ -0,0 +1,78 @@
+package tech.tfrabaioli.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+
+@Entity
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	public Categoria() {}
+	
+	
+	public Categoria(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+
+}
